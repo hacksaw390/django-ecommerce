1dce250d58ddb369d9239207ca6572d79954e139
Shamim Ahamed <shamim.ahamed@alphasq.com>
2022-07-03 17:32:45 +0600

    asdf

--- store/context-processors.py
+++ store/context-processors.py
@@ -4,5 +4,16 @@ from .models import *
 def categories(request):
     all_category = Category.objects.all()
     all_tag = Tag.objects.all()
-    context = {'all_category': all_category, 'all_tag': all_tag}
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_items': 0, 'get_cart_total': 0}
+
+    context = {'all_category': all_category, 'all_tag': all_tag, 'items': items, 'order': order}
     return context
+
+
