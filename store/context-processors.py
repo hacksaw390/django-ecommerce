03a2763a8507bbc02fdbb91d89c9567473e9f572
Shamim Ahamed <shamim.ahamed@alphasq.com>
2022-07-04 17:21:31 +0600

    asdf

--- store/context-processors.py
+++ store/context-processors.py
@@ -9,11 +9,13 @@ def categories(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else:
         items = []
-        order = {'get_cart_items': 0, 'get_cart_total': 0}
+        order = {'get_cart_items': 0, 'get_cart_total': 0, 'shipping': False}
+        cartItems = order['get_cart_items']
 
-    context = {'all_category': all_category, 'all_tag': all_tag, 'items': items, 'order': order}
+    context = {'all_category': all_category, 'all_tag': all_tag, 'items': items, 'order': order, 'cartItems': cartItems}
     return context
 
 
--- store/urls.py
+++ store/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('product-view/<int:pk>', views.product_view, name='product-view'),
     path('cart', views.cart, name='cart'),
     path('checkout', views.checkout, name='checkout'),
+
+    path('update_item', views.updateItem, name='update_item'),
 ]
